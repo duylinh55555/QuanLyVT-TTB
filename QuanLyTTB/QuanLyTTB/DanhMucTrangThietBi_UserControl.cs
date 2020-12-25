07a465a977874f362546dbf0635d48b12145b853
Linh <linh9920duy@gmail.com>
2020-12-25 15:46:43 +0700

    add main p2

---
+++ QuanLyTTB/QuanLyTTB/DanhMucTrangThietBi_UserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTTB
+{
+    public partial class DanhMucTrangThietBi_UserControl : UserControl
+    {
+        public DanhMucTrangThietBi_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
